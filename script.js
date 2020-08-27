ea8bfa3eebd613f3f7cce326c3086add2ebd92b4
ajitiok <ajitiokuswandhito@gmail.com>
2020-08-27 17:28:38 +0700

    Final Project Phase 0 - Website untuk anak-anak mengenal Traffic Light

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonmerah(){
+    document.getElementById("hasilwarna").innerHTML = 'STOP'
+}
+
+function buttonkuning(){
+    document.getElementById("hasilwarna").innerHTML = 'READY'
+}
+
+function buttonhijau(){
+    document.getElementById("hasilwarna").innerHTML = 'GO'
+}
